a9e6371f29c452b45460331f73973827f3a467de
DmytroLamashevskyi <d.lamashevskyi@gmail.com>
2025-10-21 22:24:42 +0900

    Added Toasts

--- my-todo-list/src/pages/ToDoListPage.tsx
+++ my-todo-list/src/pages/ToDoListPage.tsx
@@ -3,6 +3,7 @@ import { Form } from "../components/Form/Form";
 import { Header } from "../components/Header/Header";
 import { ToDoList } from "../components/ToDoList/ToDoList";
 import { ToDoItem } from "../models/ToDoItem";
+import { Slide, toast, ToastContainer } from "react-toastify";
 
 export const ToDoListPage = () => {
   const createNewTask = (text: string) => {
@@ -12,6 +13,7 @@ export const ToDoListPage = () => {
       isDone: false,
     };
     setTodoList([...todoList, newTask]);
+    toast.success("Task Created!");
   };
 
   const updateTask = (todoItem: ToDoItem) => {
@@ -20,11 +22,13 @@ export const ToDoListPage = () => {
       return item;
     });
     setTodoList(newToDoList);
+    toast.success("Task Updated!");
   };
 
   const deleteTask = (todoItem: ToDoItem) => {
     const newToDoList = todoList.filter((item) => item.id !== todoItem.id);
     setTodoList(newToDoList);
+    toast.error("Task Deleted!");
   };
 
   const [todoList, setTodoList] = useState<ToDoItem[]>([
@@ -49,6 +53,7 @@ export const ToDoListPage = () => {
         updateToDoTask={updateTask}
         deleteToDoTask={deleteTask}
       />
+      <ToastContainer position="bottom-right" transition={Slide} />
     </>
   );
 };
